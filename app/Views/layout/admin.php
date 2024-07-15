cae38ed8f744313188ba10c20cc83b485af68dc8
Dulhaa <rahmatabdulhakim678@gmail.com>
2024-07-15 19:43:10 +0700

    update color topbar

--- app/Views/layout/admin.php
+++ app/Views/layout/admin.php
@@ -36,11 +36,11 @@
             <div id="content">
 
                 <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <nav class="navbar navbar-expand navbar-light  topbar mb-4 static-top shadow" style="background-color:tomato;>
 
                     <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
+                    <button id= " sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                    <i class="fa fa-bars"></i>
                     </button>
 
 
