2348d1299fdc25666f03640c54f16013037615d0
Yuanta <yeremiayuanta28@gmail.com>
2024-07-15 21:56:18 +0700

    Menambah Halaman User

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -17,4 +17,12 @@ class DashboardController extends BaseController
         $data['flowersType'] = (new FlowerTypeModel())->findAll();
         return view("dashboard/index", $data);
     }
+
+    public function index2()
+    {
+        $data['totalFlowers'] = (new FlowerModel())->sumFlowerQty();
+        $data['flowers'] = (new FlowerModel())->findAll();
+        $data['flowersType'] = (new FlowerTypeModel())->findAll();
+        return view("dashboarduser/index", $data);
+    }
 }
