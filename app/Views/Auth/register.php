105ff676ca2967e0862e21e69140fa95afa909d9
= <sabriwibowoikhwan@gmail.com>
2024-07-15 18:54:22 +0700

    change title register

--- app/Views/Auth/register.php
+++ app/Views/Auth/register.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Register</title>
+    <title>TamanKu - Register</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url() ?>/template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
